9ef41c121daa71a475c092b846b8116b81179a89
AnnaRZaytseva <anna.r.zaytseva@gmail.com>
2025-07-04 15:51:17 +0300

    добавила страницу register.html

--- hr/hr/settings.py
+++ hr/hr/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from django.urls import reverse_lazy
 from pathlib import Path
+from django.core.mail import EmailMessage
 import os
+# django.contrib.sites.models import Site
+#Site.objects.create(pk=1, domain='www.example.com', name='www.example.com')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +29,7 @@ SECRET_KEY = "django-insecure-6)!k#1no!w!d1r#y*7g0+7vilowf5-&4aq*kb$cyqhsg(gff%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -36,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "employee",
@@ -121,6 +125,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+SITE_ID = 1
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -140,4 +145,12 @@ LOGIN_REDIRECT_URL = 'home'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_URL = reverse_lazy('login')
+
 LOGOUT_URL = reverse_lazy('logout')
+
+SessionEngine = 'django.contrib.sessions.backends.db'
+Session_Cookie_Name = 'sessionid'
+SESSION_COOKIE_SECURE = False
+SESSION_COOKIE_HTTPONLY = True
+SESSION_SAVE_EVERY_REQUEST = False
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
