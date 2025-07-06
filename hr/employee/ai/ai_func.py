0c75c6b1679284f674110b44f06c120ca9ed4362
AnnaRZaytseva <anna.r.zaytseva@gmail.com>
2025-07-06 23:41:32 +0300

    подправила пути кнопок "Выход"

--- hr/employee/ai/ai_func.py
+++ hr/employee/ai/ai_func.py
@@ -1,13 +1,23 @@
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_gigachat.chat_models import GigaChat
+from langchain_openai import ChatOpenAI
 import json
 import os
 import psycopg2
 import sqlite3
 
-giga = GigaChat(
-    credentials="M2Y3YmE2NTEtMTg1Zi00ZjY4LWEwOGMtYjE0ZjlhMDQ2OTYwOmE2ZDU2NmNkLTNmMTgtNDJmYi05NGU0LTc1ZDRjYjBkMmNkZg==",
-    verify_ssl_certs=False,
+# giga = GigaChat(
+#     credentials="M2Y3YmE2NTEtMTg1Zi00ZjY4LWEwOGMtYjE0ZjlhMDQ2OTYwOmE2ZDU2NmNkLTNmMTgtNDJmYi05NGU0LTc1ZDRjYjBkMmNkZg==",
+#     verify_ssl_certs=False,
+# )
+
+giga = ChatOpenAI(
+    # model="deepseek/deepseek-r1-0528-qwen3-8b:free",
+    # model="meta-llama/llama-4-maverick:free",
+    model="deepseek/deepseek-chat:free",
+    openai_api_base="https://openrouter.ai/api/v1",
+    temperature=0.2,
+    openai_api_key="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"
 )
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -35,7 +45,7 @@ def get_vacancy_info(vacancy_id):
         DB_PASSWORD = "33772"
         DB_PORT = "5432"
         
-        # conn = sqlite3.connect('db.db')  
+        # conn = sqlite3.connect('your_database.db')  
         
         conn = psycopg2.connect(dbname=DB_NAME,
                                 host=DB_HOST,
@@ -91,9 +101,9 @@ def get_q(messages):
     res = giga.invoke(messages['all'])
     messages['all'].append(res.content)
     messages[str(question)] = answer
-    print("###\n###\n###\n###")
-    print(messages)
-    print("###\n###\n###\n###")
+    # print("###\n###\n###\n###")
+    # print(messages)
+    # print("###\n###\n###\n###")
 
     return messages
 
@@ -106,11 +116,14 @@ def begin(vacancy_id):
     
     messages = {
         'all': [SystemMessage(content=system_message), ]
+        # 'all': [HumanMessage(content=system_message), ]
     }
 
     messages['all'].append(HumanMessage(content=vacancy_info))
 
     res = giga.invoke(messages['all'])
+    # print(res.content)
+    # print("TYPE:", type(res))
     messages['all'].append(res.content)
 
     return messages
--- hr/hr/views.py
+++ hr/hr/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from employee.models import Vacancy
+import json
+from django.contrib.auth import logout
 
 def index(request):
     is_hr = request.user.groups.filter(name='hr').exists()
     context = {
         'is_hr': is_hr
     }
+    vacancies = Vacancy.objects.filter(isActive=True)
+    vacancies_data = {vacancy.id:{'id':vacancy.id,
+                                  'title':vacancy.title,
+                                  'description':vacancy.description,
+                                  'requirements':vacancy.requirements,
+                                  'responsibilities':vacancy.responsibilities,
+                                  'conditions':vacancy.conditions,
+                                  'isActive':vacancy.isActive}
+                      for vacancy in vacancies}
+    if request.method == 'POST':
+        logout(request)
+        return render(request, 'main/main.html', context)
     return render(request, 'main/main.html', context)
